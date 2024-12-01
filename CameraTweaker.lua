3d18839c88685cdf54f9d6f8370967ea4966e0ef
OMGparticles <blacryu@gmail.com>
2024-11-30 17:34:51 -0800

    Added the ability to adjust camera zoom speed

--- CameraTweaker.lua
+++ CameraTweaker.lua
@@ -24,8 +24,9 @@ LoadConfig:SetScript("OnEvent", function()
     if arg1 == "CameraTweaker" then
         -- printChat("loading CameraTweaker")
         CameraTweakerDB = CameraTweakerDB or {}
-        CameraTweakerDB.yawSpeed = CameraTweakerDB.yawSpeed or GetCVar("cameraYawMoveSpeed")
-        CameraTweakerDB.pitchSpeed = CameraTweakerDB.pitchSpeed or GetCVar("cameraPitchMoveSpeed")
+        CameraTweakerDB.yawSpeed = CameraTweakerDB.yawSpeed or tonumber(GetCVar("cameraYawMoveSpeed"))
+        CameraTweakerDB.pitchSpeed = CameraTweakerDB.pitchSpeed or tonumber(GetCVar("cameraPitchMoveSpeed"))
+        CameraTweakerDB.zoomSpeed = CameraTweakerDB.zoomSpeed or tonumber(GetCVar("cameraDistanceMoveSpeed"))
     end
 end)
 
@@ -38,6 +39,9 @@ function Apply()
     local newYaw = CameraTweakerDB.yawSpeed
     local oldPitch = tonumber(GetCVar("cameraPitchMoveSpeed"))
     local newPitch = CameraTweakerDB.pitchSpeed
+    local oldZoom = tonumber(GetCVar("cameraDistanceMoveSpeed"))
+    local newZoom = CameraTweakerDB.zoomSpeed
+
     if (oldYaw ~= newYaw) then
         SetCVar("cameraYawMoveSpeed", newYaw)
         printChat("Horizontal camera speed changed from " .. oldYaw .. " to " .. newYaw .. ". Default is 180")
@@ -45,6 +49,24 @@ function Apply()
     if (oldPitch ~= newPitch) then
         SetCVar("cameraPitchMoveSpeed", newPitch)
         printChat("Vertical camera speed changed from " .. oldPitch .. " to " .. newPitch .. ". Default is 90")
+    end
+    if (oldZoom ~= newZoom) then
+        SetCVar("cameraDistanceMoveSpeed", newZoom)
+        if (newZoom <= 0.01 and tonumber(GetCVar("cameraDistanceMoveSpeed")) ~= newZoom) then
+            printChat("Zoom speed limited to 0.01 by game client.")
+            newZoom = 0.01
+            CameraTweakerDB.zoomSpeed = newZoom
+            SetCVar("cameraDistanceMoveSpeed", newZoom)
+        end
+        if (newZoom > 50 and tonumber(GetCVar("cameraDistanceMoveSpeed")) ~= newZoom) then
+            printChat("Zoom speed limited to 50 by game client.")
+            newZoom = 50
+            CameraTweakerDB.zoomSpeed = newZoom
+            SetCVar("cameraDistanceMoveSpeed", newZoom)
+        end
+        if (oldZoom ~= newZoom) then
+            printChat("Camera zooming speed changed from " .. oldZoom .. " to " .. newZoom .. ". Default is 8.33")
+        end
     end
 end
 
@@ -62,8 +84,9 @@ SlashCmdList["CAMTWEAK"] = function(input)
     iStart, iEnd = strfind(input, "[^%s]+", string.len(str1)+2)
     local str2 = strsub(input, iStart or 0, iEnd or 0)
 
-    local yawStrings = {y=true, yaw=true, h=true, horizontal=true}
-    local pitchStrings = {p=true, pitch=true, v=true, vertical=true}
+    local yawStrings = {y=true, yaw=true, yawspeed=true, h=true, horizontal=true}
+    local pitchStrings = {p=true, pitch=true, pitchspeed=true, v=true, vertical=true}
+    local zoomStrings = {z=true, zoom=true, zoomspeed=true, d=true, distance=true, distancespeed=true}
 
     if (tonumber(str1)) then -- /cam <number>
         CameraTweakerDB.yawSpeed = tonumber(str1)
@@ -71,13 +94,19 @@ SlashCmdList["CAMTWEAK"] = function(input)
         CameraTweakerDB.yawSpeed = tonumber(str2)
     elseif (pitchStrings[str1] and tonumber(str2)) then -- /cam pitch <number>
         CameraTweakerDB.pitchSpeed = tonumber(str2)
+    elseif (zoomStrings[str1] and tonumber(str2)) then -- /cam zoom <number>
+        CameraTweakerDB.zoomSpeed = tonumber(str2)
+
+
 
     -- anything else
     else
-        printChat("|cff00FF67CameraTweaker: |cff7AFFFF/cam <value> or /cam yaw <value> - Horizontal speed")
+        printChat("|cff00FF67CameraTweaker: |cff7AFFFF/ct <value> or /ct h <value> - Horizontal speed")
         printChat("|cff00FF67Current: |cffFFFFFF" .. CameraTweakerDB.yawSpeed .." |cff00FF67Default: |cffFF8888180")
-        printChat("|cff00FF67CameraTweaker: |cff7AFFFF/cam pitch <value> - Vertical speed")
+        printChat("|cff00FF67CameraTweaker: |cff7AFFFF/ct v <value> - Vertical speed")
         printChat("|cff00FF67Current: |cffFFFFFF" .. CameraTweakerDB.pitchSpeed .." |cff00FF67Default: |cffFF888890")
+        printChat("|cff00FF67CameraTweaker: |cff7AFFFF/ct z <value> - Zoom in/out speed")
+        printChat("|cff00FF67Current: |cffFFFFFF" .. CameraTweakerDB.zoomSpeed .." |cff00FF67Default: |cffFF88888.33")
     end
     Apply()
 end
